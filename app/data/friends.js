6e6d5b494fe53df6eb744312787fe21bf0cdc1c4
anishbnair <nbanish@gmail.com>
2018-02-28 19:33:05 -0500

    included a dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// ===============================================================================
+// DATA
+// ===============================================================================
+
+var userData = [
+    {
+      "name": "John",
+      "photo": "",
+      "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+      ]
+    },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = userData;
