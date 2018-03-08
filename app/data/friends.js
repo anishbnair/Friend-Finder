84453be756adb4b9f4843f7fc8d1749e70a6c877
anishbnair <nbanish@gmail.com>
2018-03-07 20:05:48 -0500

    included couple more friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,38 @@ var userData = [
             2,
             5,
             4,
+            3
+      ]
+    },
+    {
+      "name": "John",
+      "photo": "https://avatars2.githubusercontent.com/u/18151398?s=400&u=abb84cdc0bf9f20543366395484bc72ec954dcc0&v=4",
+      "scores":[
+            1,
+            5,
+            2,
+            3,
+            3,
+            4,
+            5,
+            2,
+            1,
+            1
+      ]
+    },
+    {
+      "name": "Dave",
+      "photo": "https://avatars2.githubusercontent.com/u/18151398?s=400&u=abb84cdc0bf9f20543366395484bc72ec954dcc0&v=4",
+      "scores":[
+            3,
+            3,
+            2,
+            1,
+            5,
+            1,
+            4,
+            2,
+            3,
             1
       ]
     },
