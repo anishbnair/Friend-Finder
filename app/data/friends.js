734a4f32a385cabcc5351267618d4c79410c78a2
anishbnair <nbanish@gmail.com>
2018-03-07 20:56:15 -0500

    added one more user data

--- app/data/friends.js
+++ app/data/friends.js
@@ -51,6 +51,22 @@ var userData = [
             1
       ]
     },
+    {
+      "name": "Matt",
+      "photo": "https://avatars2.githubusercontent.com/u/18151398?s=400&u=abb84cdc0bf9f20543366395484bc72ec954dcc0&v=4",
+      "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+      ]
+    },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
